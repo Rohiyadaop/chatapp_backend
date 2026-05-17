32091ec1ece322c344dabebeba636b0621ea5c4a
Rohit Yadav <rohityadav@gmail.com>
2026-05-17 23:56:49 +0530

    t

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 // Setup Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://app-chat-delta.vercel.app",
     methods: ["GET", "POST"],
   },
 });
